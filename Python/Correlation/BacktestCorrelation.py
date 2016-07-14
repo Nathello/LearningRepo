8f31c38ed56a64bcd0605ac3447013bd8e9a1030
Nathello <chrisdrydenwork@gmail.com>
2016-07-14 12:53:38 +0100

    Legend for chart sorted

--- Python/Correlation/BacktestCorrelation.py
+++ Python/Correlation/BacktestCorrelation.py
@@ -28,7 +28,8 @@ reporting_period = np.arange(len(reported_figures))
 merchant_spend_series = merchant_spend.plot(reporting_period, reported_figures, "b-", label="Reported Figure")
 xlabels = test_file["Period"]
 plt.xticks(reporting_period, xlabels, rotation = 45)
-merchant_spend_legend = merchant_spend.legend(loc='upper left', bbox_to_anchor=(0.5, 0.5))
+merchant_spend_legend = merchant_spend.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
 
 
 mdb_spend = merchant_spend.twinx()
@@ -36,7 +37,7 @@ mdb_spend.set(ylabel=("MDB Spend(£000s)"))
 mdb_spend.get_yaxis().set_major_formatter(FuncFormatter(lambda x, p: format(int(x/1000), ',')))
 mdb_spend_figures = test_file["MDB Spend Figures"]
 mdb_spend_series = mdb_spend.plot(reporting_period, mdb_spend_figures, "r")
-mdb_spend_legend = mdb_spend.legend(loc='center left')
+mdb_spend_legend = mdb_spend.legend(frameon=False, bbox_to_anchor=(0., 1.006, 1., .1), loc='center right', borderaxespad=0.)
 
 data_table = fig.add_subplot(2,2,2)
 data_table.axis('off')
@@ -58,6 +59,6 @@ r_sq = r ** 2
 plt.annotate("r^2 = {0:.2f}".format(r_sq), (0.05, 0.9), xycoords="axes fraction")
 plt.annotate("P Value = {0:.4f}".format(p_value), (0.05, 0.85), xycoords="axes fraction")
 
-plt.subplots_adjust(top=0.92, bottom=0.04, left=0.09, right=0.97, hspace=0.4, wspace=0.61)
+plt.subplots_adjust(top=0.9, bottom=0.04, left=0.09, right=0.97, hspace=0.4, wspace=0.61)
 
 plt.show()
